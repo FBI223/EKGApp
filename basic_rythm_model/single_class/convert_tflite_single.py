d32ac7891f1c1391db9a8b0c6072a30c0cdc1229
Marcin Sztukowski <sztukowski@vp.pl>
2025-06-04 03:00:19 +0200

    android - mostly final version of app

--- basic_rythm_model/single_class/convert_tflite_single.py
+++ basic_rythm_model/single_class/convert_tflite_single.py
@@ -1,17 +1,19 @@
-
 import tensorflow as tf
-
 
 SAVE_NAME = "model_mobile_single"
 
-# Załaduj zapisany model
-converter = tf.lite.TFLiteConverter.from_saved_model( SAVE_NAME + "_tf")
+converter = tf.lite.TFLiteConverter.from_saved_model("model_no_lstm_final/" + SAVE_NAME + "_tf")
 
-# Konwertuj do TFLite (float32)
+# Make sure only supported ops and types are used (mostly float ops)
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
+
+# ensure inference input/output types are float32
+converter.inference_input_type = tf.float32
+converter.inference_output_type = tf.float32
+
 tflite_model = converter.convert()
 
-# Zapisz do pliku
-with open( SAVE_NAME + ".tflite", "wb") as f:
+with open(SAVE_NAME + ".tflite", "wb") as f:
     f.write(tflite_model)
 
 print("✅ Konwersja do model.tflite zakończona")
